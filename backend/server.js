98ac3d50b7fc11e12fd40e2ce336688708011052
Kotesh-Pusala <pusalakotesh08@gmail.com>
2025-04-27 16:17:15 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import cors from 'cors';
+
+import productRoutes from './routes/product.route.js';
+
+dotenv.config();
+
+const app = express();
+
+// 👉 THIS LINE WAS MISSING
+connectDB(); 
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
